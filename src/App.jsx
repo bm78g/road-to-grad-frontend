c356fb97330d0c27acfbcbc4a199482bccfe53fb
bm78g <73971640+bm78g@users.noreply.github.com>
2026-01-14 17:45:12 -0600

    feat: tip bubble

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import './App.css'
 import CSGraph from './components/CSHome/CSGraph'
-import csLogo from './assets/pc-logo-placeholder.png'
+import rtgLogo from './assets/roadtograd-logo.png'
+import csLogo from './assets/question-logo.png'
 
 function App() {
   return (
     <>
       <div className='header-container'>
         <div className='header-content'>
-          <img src={csLogo} className='cs-logo'></img>
-          <h1>Computer Science Catalog</h1>
+          <img src={ rtgLogo } className='rtg-logo'></img>
         </div>
         <hr></hr>
+      </div>
+      <div className='graph-header'>
+        <h1>Computer Science Catalog</h1>
+        <div className='tooltip'>
+          <img src={ csLogo } className='tip-logo'></img>
+          <div className='tip-bubble'>
+            <h1>Keep Note:</h1>
+            <p>Cross-listed courses are omitted, IE: CS 3345 vs CE 3345</p>
+          </div>
+        </div>
       </div>
       <CSGraph />
     </>
